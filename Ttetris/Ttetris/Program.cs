f206198e7bc39a647dd2ed213b53d22b71cacccc
mikhail <esto045@gmail.com>
2021-10-03 10:02:11 +0500

    Update Program.cs
    упс

--- Ttetris/Ttetris/Program.cs
+++ Ttetris/Ttetris/Program.cs
@@ -13,19 +13,19 @@ namespace Ttetris
             int y1 = 3;
             char c1 = '*';
 
-            Drow(x1, y1, c1);
+            Draw(x1, y1, c1);
 
             int x2 = 4;
             int y2 = 5;
             char c2 = '@';
 
-            Drow(x2, y2, c2);
+            Draw(x2, y2, c2);
 
 
             Console.ReadLine();
         }
 
-        static void Drow(int x, int y, char c)
+        static void Draw(int x, int y, char c)
         {
             Console.SetCursorPosition(x, y);
             Console.WriteLine(c);
